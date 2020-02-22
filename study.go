27d188ddd21cdb404323a5b1b66b18403e5ed6e8
dorayaki <ytoriyama@gmail.com>
2020-02-22 21:20:15 +0900

    switch study

--- study.go
+++ study.go
@@ -2,33 +2,25 @@ package main
 
 import "fmt"
 
-// Person は人間を表す構造体。
-type Person struct {
-	Name string
-	Age  int
-}
-
 func main() {
+	s := "A"
+	switch s {
+	case "A":
+		s += "B"
+		fallthrough
+	case "B":
+		s += "C"
+		fallthrough
+	case "C":
+		s += "D"
+		fallthrough
+	case "D":
+		s += "E"
+		fallthrough
 
-	// 値として、pに代入
-	p := Person{
-		Name: "太郎",
-		Age:  20,
+	default:
+		s += "E"
+		fmt.Println(s)
 	}
 
-	fmt.Printf("最初のp :%+v\n", p)
-
-	p2 := p
-	p2.Name = "二郎"
-	p2.Age = 21
-	// pではなく
-	fmt.Printf("p2で二郎に書き換えを行なったはずのp :%+v\n", p)
-
-	// &pで*Person(Personのポインタ型)を生成する
-	// p3はpのアドレスが格納されている状態になる
-	p3 := &p
-	p3.Name = "二郎"
-	p3.Age = 21
-
-	fmt.Printf("p3で二郎に書き換えを行なったp :%+v\n", p)
 }
