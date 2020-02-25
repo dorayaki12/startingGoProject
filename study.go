c4d59eea767b9af40aa54dd333a4cf3bbeee9805
dorayaki <ytoriyama@gmail.com>
2020-02-25 16:33:13 +0900

    スライスの学習

--- study.go
+++ study.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 func main() {
-	var x interface{} = 3
+	var x interface{} = "mo"
 
 	/* 変数xはinterface{}型 */
 	if x == nil {
@@ -17,5 +18,30 @@ func main() {
 	} else {
 		fmt.Println("unsupported type")
 	}
+L:
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 3; j++ {
+			if j > 1 {
+				continue L
+
+			}
+			fmt.Printf("%d * %d = %d\n", i, j, i*j)
+		}
+		fmt.Println("ここは処理されない")
+	}
+
+	go fmt.Println("hahaha")
+
+	fmt.Printf("NumCPU: %d\n", runtime.NumCPU())
+	fmt.Printf("NumGoroutine: %d\n", runtime.NumGoroutine())
+	fmt.Printf("goVersion: %s\n", runtime.Version())
+
+	s1 := make([]int, 5)
+	fmt.Println(len(s1))
+	fmt.Println(cap(s1))
+
+	s2 := make([]int, 5, 10)
+	fmt.Println(len(s2))
+	fmt.Println(cap(s2))
 
 }
