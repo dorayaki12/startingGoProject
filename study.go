08ab8aff8acb9a8a6a5514cd148879868df13631
dorayaki <ytoriyama@gmail.com>
2020-02-27 17:26:56 +0900

    ゴルーチンとチャンネル

--- study.go
+++ study.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func receive(name string, ch <-chan int) {
@@ -14,26 +13,26 @@ func receive(name string, ch <-chan int) {
 		fmt.Println(name, i)
 	}
 	fmt.Println(name + "is done")
-
 }
 
 func main() {
-	ch := make(chan int, 20)
+	ch1 := make(chan int, 1)
+	ch2 := make(chan int, 2)
+	ch3 := make(chan int, 3)
 
-	go receive("1st goroutine", ch)
-	go receive("2nd goroutine", ch)
-	go receive("3rd goroutine", ch)
+	ch1 <- 1
+	ch2 <- 2
+	ch3 <- 3
 
-	i := 0
-	for i < 100 {
-		ch <- i
-		i++
-		time.Sleep(50 * time.Millisecond)
+	// 複数のcaseが成立する場合はランダムに選択される
+	select {
+	case <-ch1:
+		fmt.Println("ch1から受信")
+	case <-ch2:
+		fmt.Println("ch2から受信")
+	case <-ch3:
+		fmt.Println("ch3から受信")
+	default:
+		fmt.Println("ここには到達しない")
 	}
-
-	close(ch)
-
-	// ゴルーチンの完了を３秒待つ
-	time.Sleep(3 * time.Second)
-
 }
