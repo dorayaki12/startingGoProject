96b2c48f8f7b1ab90dddcd9fa5336d50395bacb9
dorayaki <ytoriyama@gmail.com>
2020-03-05 14:47:15 +0900

    配列とポインタ１

--- study.go
+++ study.go
@@ -4,24 +4,22 @@ import (
 	"fmt"
 )
 
+func inc(p *int) {
+	*p++
+}
+
 func main() {
-	ch1 := make(chan int, 1)
-	ch2 := make(chan int, 2)
-	ch3 := make(chan int, 3)
+	var i int
+	p := &i
+	i = 5
+	fmt.Println(*p)
 
-	ch1 <- 1
-	ch2 <- 2
-	ch3 <- 3
+	inc(&i)
+	inc(&i)
+	inc(&i)
+	inc(&i)
+	inc(&i)
 
-	// 複数のcaseが成立する場合はランダムに選択される
-	select {
-	case <-ch1:
-		fmt.Println("ch1から受信")
-	case <-ch2:
-		fmt.Println("ch2から受信")
-	case <-ch3:
-		fmt.Println("ch3から受信")
-	default:
-		fmt.Println("ここには到達しない")
-	}
+	fmt.Println(i)
+	fmt.Println(p)
 }
